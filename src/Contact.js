bd7b6da89f24c86c75ff9646618668b8efe80bb5
zhkate <kz2554@columbia.edu>
2026-02-20 11:41:24 -0500

    update ab

--- src/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Contact() {
-  return <h1 style={pageStyle}>Contact Us</h1>;
-}
-
-const pageStyle = { textAlign: 'center', marginTop: '100px' };
-
-export default Contact;
